f7857d3c7e49ed821c9b556944a6c86e40ada857
PRANESH SHARMA <22mc3042@rgipt.ac.in>
2024-11-17 22:21:40 +0530

    wifi

--- wifi_connect_and_login.sh
+++ wifi_connect_and_login.sh
@@ -1,41 +1,9 @@
-#WIFI_SSID="RGIPT_WLAN"
-#nmcli dev wifi connect "$WIFI_SSID"
-#sleep 5
-#LOGIN_URL="http://192.168.100.1:8090/"
-#username="22mc3042"
-#password="pss@123"
+#wifi ssid/name
 
-#firefox "$LOGIN_URL" &
-#this will open the browser 
-
-#sleep 1 
-
-#xdotool search --sync --onlyvisible --class "firefox" windowactivate
-
-#sleep 1
-
-#xdotool key Tab
-
-#sleep 1
-#xdotool type "$username"
-
-#xdotool key Tab
-
-#sleep 1
-
-#xdotool type "$password"
-
-#xdotool key Return
-
-
-# NEW         SCRIPT      HELOOOOOOOOOO
-
-
-#!/bin/bash
 WIFI_SSID="RGIPT_WLAN"
 LOGIN_URL="http://192.168.100.1:8090/"
-username="22mc3042"
-password="pss@123"
+username="##"
+password="##"
 
 while true; do
     CONNECTION_STATUS=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | grep -w "$WIFI_SSID")
